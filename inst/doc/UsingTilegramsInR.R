75b2701470668c86de5c0c90920da2702aa0dab0
Bhaskar Karambelkar <bhaskarvk@gmail.com>
2017-03-29 16:43:16 +0000

    version 0.2.0

---
+++ inst/doc/UsingTilegramsInR.R
@@ -0,0 +1,7 @@
+## ----eval=FALSE----------------------------------------------------------
+#  install.packages('tilegramsR')
+#  
+#  #OR Latest Development version
+#  
+#  devtools::install_github('bhaskarvk/tilegramsR')
+
